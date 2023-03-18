75e332014f6abf8505d908ce7bdb065a18bdeb3b
rsugaya <rsugaya@llc-noa.net>
2023-03-18 15:16:53 +0900

    Initial Commit

---
+++ CameraApp/CameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraAppApp.swift
+//  CameraApp
+//
+//  Created by 菅谷亮太 on 2023/03/18.
+//
+
+import SwiftUI
+
+@main
+struct CameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
